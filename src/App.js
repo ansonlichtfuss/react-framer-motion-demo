665a5181dfefeb7e1365a40c3228db6e04bf50be
Anson <git@lichtf.us>
2020-04-03 07:37:51 -0600

    refactor: remove warning, clean up code preparing for animations

--- src/App.js
+++ src/App.js
@@ -18,38 +18,50 @@ function App() {
         <div className="flex flex-col items-center">
           <small>Emoji</small>
           <Switch
-            defaultOn={emojiVisible}
-            switchFlipped={status => setEmojiVisible(status)}
+            switchedOn={emojiVisible}
+            switchClicked={status => setEmojiVisible(status)}
           />
         </div>
         <div className="flex flex-col items-center">
           <small>Yogurt Brands</small>
           <Switch
-            defaultOn={yogurtBrandsVisible}
-            switchFlipped={status => setYogurtBrandsVisible(status)}
+            switchedOn={yogurtBrandsVisible}
+            switchClicked={status => setYogurtBrandsVisible(status)}
           />
         </div>
       </div>
       <div className="section-1 my-3">
-        <h2 className="text-2xl mb-2">Emoji</h2>
-        {emojiVisible && (
-          <div className="flex justify-around">
-            {emoji.map(thisEmoji => (
+        <h2
+          className="text-2xl mb-2 transition ease-in-out duration-200"
+          style={{ opacity: emojiVisible ? 1 : 0.5 }}
+        >
+          Emoji
+        </h2>
+        <div className="flex justify-around h-32">
+          {emojiVisible &&
+            emoji.map(thisEmoji => (
               <div className="shadow rounded bg-white w-32 h-32 flex items-center justify-center text-6xl">
                 {thisEmoji}
               </div>
             ))}
-          </div>
-        )}
+        </div>
       </div>
       <div className="section-2 my-6">
-        <h2 className="text-2xl">Yogurt Brands</h2>
-        {yogurtBrandsVisible &&
-          yogurtBrands.map(brand => (
-            <div className="shadow rounded bg-white h-auto p-4 my-2 overflow-hidden text-black">
-              {brand}
-            </div>
-          ))}
+        <h2
+          className="text-2xl transition ease-in-out duration-200 "
+          style={{ opacity: yogurtBrandsVisible ? 1 : 0.5 }}
+        >
+          Yogurt Brands
+        </h2>
+        {yogurtBrandsVisible && (
+          <ul>
+            {yogurtBrands.map(brand => (
+              <li className="shadow rounded bg-white h-auto p-4 my-2 overflow-hidden text-black">
+                {brand}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Switch({ defaultOn = false, switchFlipped = () => {} }) {
-  const [switchedOn, setSwitchedOn] = useState(defaultOn);
-
-  const switchClicked = () => {
-    setSwitchedOn(current => {
-      switchFlipped(!current);
-      return !current;
-    });
-  };
-
+function Switch({ switchedOn = false, switchClicked = () => {} }) {
   const toggleClass = switchedOn ? 'ml-auto mr-1' : 'mr-auto ml-1';
 
   return (
     <div
-      className="rounded-full bg-green-400 bg-gray-200 w-16 h-8 shadow-inner flex items-center mt-2 cursor-pointer"
-      onClick={switchClicked}
+      className="rounded-full hover:bg-green-500 transition ease-in-out duration-200 bg-green-400 bg-gray-200 w-16 h-8 shadow-inner flex items-center mt-2 cursor-pointer"
+      onClick={() => switchClicked(!switchedOn)}
     >
       <div
         className={`rounded-full bg-white w-6 h-6 shadow ${toggleClass}`}
